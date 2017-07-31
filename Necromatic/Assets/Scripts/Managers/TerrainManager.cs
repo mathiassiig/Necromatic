08061b526cf2d8f4e14495e0808b12a2e1e01f96
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2017-07-31 23:16:46 +0300

    Basic object pooling

---
+++ Necromatic/Assets/Scripts/Managers/TerrainManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainManager : MonoBehaviour
+{
+    [SerializeField] private Transform _groundRoot;
+    [SerializeField] private ObjectPool _groundTilePool;
+
+    private const int _chunkSizeX = 8;
+    private const int _chunkSizeZ = 8;
+    public void Awake()
+    {
+        var chunk = GenerateChunk();
+        chunk.transform.SetParent(_groundRoot);
+    }
+
+    public GameObject GenerateChunk()
+    {
+        var chunk = new GameObject();
+        for (int x = 0; x < _chunkSizeX; x++)
+        {
+            for (int z = 0; z < _chunkSizeZ; z++)
+            {
+                InitGround(x, z, chunk.transform);
+            }
+        }
+        return chunk;
+    }
+
+    private void InitGround(float x, float z, Transform parent)
+    {
+        var ground = _groundTilePool.PoolObject();
+        ground.transform.SetParent(parent);
+        ground.transform.position = new Vector3(x, 0, z);
+    }
+}
---
+++ Necromatic/Assets/Scripts/Utility/ObjectPool.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] private GameObject _prefab;
+    [Tooltip("Where to put the pooled objects. If this isn't set, the objects will be directly under this gameobject")]
+    [SerializeField] private Transform _activeCellContainer;
+    [SerializeField] private int _initialActiveCellsSize;
+    [SerializeField] private int _recycledCellsSize; // size of disabled objects
+
+    private Transform _recycledObjectRoot;
+    private Queue<GameObject> _recycledObjects = new Queue<GameObject>();
+    
+    void Awake()
+    {
+        InitRecycleTransform();
+    }
+
+    void InitRecycleTransform()
+    {
+        var recycler = new GameObject();
+        recycler.transform.SetParent(transform);
+        _recycledObjectRoot = recycler.transform;
+    }
+
+    public void CacheObject(GameObject obj)
+    {
+        if (_recycledObjects.Count < _recycledCellsSize)
+        {
+            _recycledObjects.Enqueue(obj);
+            obj.transform.SetParent(_recycledObjectRoot);
+            obj.SetActive(false);
+        }
+        else
+        {
+            Destroy(obj);
+        }
+    }
+
+    public GameObject PoolObject()
+    {
+        if(_recycledObjects.Count > 0)
+        {
+            return _recycledObjects.Dequeue();
+        }
+        else
+        {
+            return Instantiate(_prefab, _activeCellContainer ?? transform);
+        }
+    }
+}
