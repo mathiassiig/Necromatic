3b93bcde91e45620a84a9743d110f15bfd5a1122
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2017-08-03 21:52:07 +0300

    UniRX what's up

---
+++ Necromatic/Assets/Scripts/Character/Stat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat
+{
+    public float Max;
+    public float Current;
+    public float Regen; // per second
+
+    public void SetToMax()
+    {
+        Current = Max;
+    }
+
+    public void Add(float value)
+    {
+        Current += value;
+    }
+}
