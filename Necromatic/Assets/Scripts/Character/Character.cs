ee2ce1d41436adb4c92cf92c199b680b6139d420
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2017-08-05 16:58:12 +0300

    Added a healthbar

--- Necromatic/Assets/Scripts/Character/Character.cs
+++ Necromatic/Assets/Scripts/Character/Character.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using Necromatic.UI;
 namespace Necromatic.Character
 {
     public class Character : MonoBehaviour
     {
         public Stat Health;
+        [SerializeField] private Vector3 _healthBarOffset = new Vector3(0, 2, 0);
+
+        private StatBar _healthBar;
 
         public ReactiveProperty<bool> IsDead = new ReactiveProperty<bool>();
 
@@ -15,8 +19,20 @@ namespace Necromatic.Character
             Init();
         }
 
+        private void InitBar()
+        {
+            var prefab = Resources.Load<StatBar>("Prefabs/UI/StatBar");
+            _healthBar = Instantiate(prefab);
+            transform.ObserveEveryValueChanged(t=>t.position).Subscribe(pos =>
+            {
+                _healthBar.transform.position = pos + _healthBarOffset;
+            });
+            _healthBar.Init(Color.green, Color.red);
+        }
+
         protected virtual void Init()
         {
+            InitBar();
             Health.Init();
             IsDead.Subscribe(x =>
             {
@@ -25,11 +41,13 @@ namespace Necromatic.Character
             Health.Current.Subscribe(x =>
             {
                 if (x <= 0) IsDead.Value = true;
+                _healthBar.UpdateStatbar(Health.Max.Value, Health.Current.Value);
             });
         }
 
         protected virtual void HandleDeath()
         {
+            Destroy(_healthBar.gameObject);
             Destroy(gameObject);
         }
     }
---
+++ Necromatic/Assets/Scripts/UI/StatBar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Necromatic.UI
+{
+
+    public class StatBar : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer _foreground;
+        [SerializeField] private float _barWidth = 10;
+        private Color _colorMax;
+        private Color _colorMin;
+
+
+
+        public void Init(Color colorMax, Color colorMin)
+        {
+            _foreground.color = colorMax;
+            _colorMax = colorMax;
+            _colorMin = colorMin;
+        }
+
+        public void UpdateStatbar(float max, float current)
+        {
+            float t = Mathf.Clamp(current / max, 0, 1);
+            float width = Mathf.Lerp(0, _barWidth, t);
+            Color c = Color.Lerp(_colorMin, _colorMax, t);
+            _foreground.color = c;
+            _foreground.size = new Vector2(width, _foreground.size.y);
+
+        }
+    }
+
+}
