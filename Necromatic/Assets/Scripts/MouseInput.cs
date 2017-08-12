a90307ef51a7572016694101216c0a825c1639b0
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2017-08-12 18:07:23 +0300

    Mouse input class

---
+++ Necromatic/Assets/Scripts/MouseInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Necromatic
+{
+    public class MouseInput : MonoBehaviour
+    {
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            CheckRightClick();
+        }
+
+        void CheckRightClick()
+        {
+            if (Input.GetMouseButton(1))
+            {
+                // create a ray cast and set it to the mouses cursor position in game
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    // Debug.DrawLine(ray.origin, hit.point);
+                    // if there is nothing, the user probably wants to attack the nearest enemy?
+                    // if there is an enemy, attack it
+                    // if there is something else, figure out what to do with it
+                }
+            }
+        }
+    }
+}
