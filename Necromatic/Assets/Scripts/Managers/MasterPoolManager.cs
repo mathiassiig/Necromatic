5cdb1df6ce20a76b850d077f1121086b6235edf8
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2017-08-12 15:38:05 +0300

    Basic object instantiation manager

---
+++ Necromatic/Assets/Scripts/Managers/MasterPoolManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+namespace Necromatic
+{
+    public enum CharacterType
+    {
+        NPCUndeadInfantry,
+        NPCHumanInfantry
+    }
+
+    public enum CorpseType
+    {
+        CorpseHumanInfantry
+    }
+
+    public class MasterPoolManager : Singleton<MasterPoolManager>
+    {
+        protected MasterPoolManager() { }
+
+        private const string _prefabLocation = "Prefabs/";
+
+        public Dictionary<CorpseType, string> Corpses = new Dictionary<CorpseType, string>
+        {
+            { CorpseType.CorpseHumanInfantry,   $"{_prefabLocation}/Corpses/Human/Corpse_Human_Infantry" }
+        };
+
+        public Dictionary<CharacterType, string> Characters = new Dictionary<CharacterType, string>
+        {
+            { CharacterType.NPCHumanInfantry,   $"{_prefabLocation}/Characters/Human/NPC_Human_Infantry" },
+            { CharacterType.NPCUndeadInfantry,  $"{_prefabLocation}/Characters/Undead/NPC_Undead_Infantry" }
+        };
+
+        // todo: pooling
+
+        public GameObject GetCorpse(CorpseType c)
+        {
+            return null;
+        }
+
+        public GameObject GetCharacter(CharacterType c)
+        {
+            var location = Characters[c];
+            var prefab = Resources.Load(location);
+            return Instantiate(prefab) as GameObject;
+        }
+    }
+}
