e3a532f0238a114cc04af76f8e12e8f6bba3aa14
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2017-08-12 17:56:11 +0300

    Tweaking the gibber animations

--- Necromatic/Assets/Scripts/Character/Gibber.cs
+++ Necromatic/Assets/Scripts/Character/Gibber.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
+using UniRx;
+using System;
 public class Gibber : MonoBehaviour
 {
     [SerializeField] private Sprite[] _bloodSprites;
@@ -16,18 +18,25 @@ public class Gibber : MonoBehaviour
 
     public void Gib()
     {
+        transform.SetParent(null);
+        _spriteRenderer.gameObject.transform.localScale = Vector3.zero;
+        _spriteRenderer.gameObject.transform.DOScale(1, 0.15f).SetEase(Ease.OutExpo);
         _spriteRenderer.gameObject.SetActive(true);
         _spriteRenderer.sprite = RandomSprite();
         foreach (var ps in _particles)
         {
             ps.Play();
         }
+        Observable.Timer(TimeSpan.FromSeconds(5)).First().Subscribe(_ =>
+        {
+            _spriteRenderer.DOFade(0, 2f).OnComplete(() => Destroy(gameObject));
+        });
     }
 
     private Sprite RandomSprite()
     {
         int max = _bloodSprites.Length;
-        int random = Random.Range(0, max);
+        int random = UnityEngine.Random.Range(0, max);
         return _bloodSprites[random];
     }
 }
