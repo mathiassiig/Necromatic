8a5d7d23202b11d3e35ece4a6506f5633ea51c08
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2017-08-13 23:29:00 +0300

    Fix rotation-bug on player

--- Necromatic/Assets/Scripts/Character/CharacterMovement.cs
+++ Necromatic/Assets/Scripts/Character/CharacterMovement.cs
@@ -17,24 +17,18 @@ namespace Necromatic.Char
         [SerializeField]
         private Rigidbody m_Rigidbody;
         [SerializeField]
-        private CapsuleCollider m_Capsule;
-        [SerializeField]
         private float m_TurnSpeed = 10f;
         [SerializeField]
         private CharacterCombat _combat;
-        private const float k_Half = 0.5f;
+        [SerializeField]
+        private Transform _transformToRotate;
         private float m_ForwardAmount;
-        private Vector3 m_GroundNormal;
-        private float m_CapsuleHeight;
-        private Vector3 m_CapsuleCenter;
+        
 
 
         private void Start()
         {
             m_Rigidbody = GetComponent<Rigidbody>();
-            m_Capsule = GetComponent<CapsuleCollider>();
-            m_CapsuleHeight = m_Capsule.height;
-            m_CapsuleCenter = m_Capsule.center;
         }
 
         private void OnEnable()
@@ -47,18 +41,18 @@ namespace Necromatic.Char
             var rawMove = move;
             move.Normalize();
             move = transform.InverseTransformDirection(move);
-            move = Vector3.ProjectOnPlane(move, m_GroundNormal);
-            m_ForwardAmount = move.z;
+            move = Vector3.ProjectOnPlane(move, Vector3.zero);
+            m_ForwardAmount = move.magnitude;
             UpdateAnimator(move, rawMove);
         }
 
 
         private void TurnTowards(Transform t)
         {
-            var currentRotation = transform.rotation;
-            transform.LookAt(t);
-            var newRotation = transform.rotation;
-            transform.rotation = Quaternion.Lerp(currentRotation, newRotation, m_TurnSpeed * Time.deltaTime);
+            var currentRotation = _transformToRotate.rotation;
+            _transformToRotate.LookAt(t);
+            var newRotation = _transformToRotate.rotation;
+            _transformToRotate.rotation = Quaternion.Lerp(currentRotation, newRotation, m_TurnSpeed * Time.deltaTime);
         }
 
         private void UpdateAnimator(Vector3 move, Vector3 rawMove)
@@ -75,7 +69,7 @@ namespace Necromatic.Char
             }
             if (rawMove != Vector3.zero)
             {
-                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, Mathf.Atan2(rawMove.x, rawMove.z) * Mathf.Rad2Deg, 0),
+                _transformToRotate.rotation = Quaternion.Lerp(_transformToRotate.rotation, Quaternion.Euler(0, Mathf.Atan2(rawMove.x, rawMove.z) * Mathf.Rad2Deg, 0),
                     m_TurnSpeed * Time.deltaTime);
             }
             m_Animator.SetFloat("Forward", m_ForwardAmount, 0.1f, Time.deltaTime);
