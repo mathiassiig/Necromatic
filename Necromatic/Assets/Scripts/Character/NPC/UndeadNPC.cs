79562841b9b6ed036b94e9ad7ea79b2cdff236cf
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2017-08-14 19:59:26 +0300

    Classic Command and Conquer style of moving dead NPCs underground

--- Necromatic/Assets/Scripts/Character/NPC/UndeadNPC.cs
+++ Necromatic/Assets/Scripts/Character/NPC/UndeadNPC.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Necromatic.Char.Combat;
+using DG.Tweening;
+using UniRx;
+using System;
+
 namespace Necromatic.Char.NPC
 {
     public class UndeadNPC : CharacterNPC
     {
+        private const float _timeUntilDisappear = 3f;
+        private const float _amountToMoveUnderground = 3f;
         void Awake()
         {
             Init();
@@ -16,6 +22,12 @@ namespace Necromatic.Char.NPC
             enabled = false;
             Movement.M_Animator.SetTrigger("Death");
             Movement.M_Animator.SetBool("Dead", true);
+            Observable.Timer(TimeSpan.FromSeconds(_timeUntilDisappear * Time.timeScale))
+                .Subscribe(_ =>
+                {
+                    transform.DOMoveY(transform.position.y - _amountToMoveUnderground, _timeUntilDisappear)
+                    .SetEase(Ease.InExpo);
+                });
         }
     }
 }
