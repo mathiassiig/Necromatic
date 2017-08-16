ab04126951bc5eeedabe6d4ba3bdc222b513b91f
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2017-08-16 21:30:01 +0300

    A tree that can be timbered

---
+++ Necromatic/Assets/Scripts/World/ResourceTree.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceTree : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _log;
+
+    private void Awake()
+    {
+        Timber(1.5f * Vector3.left);
+    }
+
+    public void Timber(Vector3 force)
+    {
+        _log.constraints = RigidbodyConstraints.None;
+        _log.AddForce(force, ForceMode.Impulse);
+    }
+}
