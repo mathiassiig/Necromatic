c5768060d60717bb226a247f99180704188faa03
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2018-03-07 22:51:31 +0200

    Moved weapon to the right

--- NecromaticUnity/Assets/Scripts/Character/WeaponAnimator.cs
+++ NecromaticUnity/Assets/Scripts/Character/WeaponAnimator.cs
@@ -7,19 +7,21 @@ namespace Necromatic.Character
 {
     public class WeaponAnimator
     {
+        private Vector3 _retracted = new Vector3(0.25f, 0, 1.8f);
+        private Vector3 _forward = new Vector3(0.25f, 0, 3f);
         public void FireAnimation(CombatState state, Transform weapon, Combat c)
         {
             weapon.DOKill();
             switch (state)
             {
                 case CombatState.Idle:
-                    weapon.localPosition = new Vector3(0, 0, 1.8f);
-					break;
+                    weapon.localPosition = _retracted;
+                    break;
                 case CombatState.Forward:
-                    weapon.DOLocalMove(new Vector3(0, 0, 3), c.ForwardTime).SetEase(Ease.OutExpo);
+                    weapon.DOLocalMove(_forward, c.ForwardTime).SetEase(Ease.OutExpo);
                     break;
                 case CombatState.Retracting:
-                    weapon.DOLocalMove(new Vector3(0, 0, 1.8f), c.RetractTime).SetEase(Ease.OutExpo);
+                    weapon.DOLocalMove(_retracted, c.RetractTime).SetEase(Ease.OutExpo);
                     break;
             }
         }
