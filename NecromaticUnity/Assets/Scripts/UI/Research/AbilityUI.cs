9037d087a31b337a3471dd4c4f1668c2a93de37c
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2018-03-11 19:12:32 +0200

    WIP research ui

---
+++ NecromaticUnity/Assets/Scripts/UI/Research/AbilityUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+namespace Necromatic.UI.Research
+{
+    public class AbilityUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _cost;
+        [SerializeField] private TextMeshProUGUI _name;
+        [SerializeField] private Image _iconImage;
+        [SerializeField] private Button _button;
+
+        public void Init(ResearchData data)
+        {
+            _name.text = data.Name;
+            _cost.text = data.Cost.ToString();
+            _iconImage.sprite = Resources.Load<Sprite>(data.IconPath);
+            Material mat = Instantiate(_iconImage.material);
+            _iconImage.material = mat;
+            _iconImage.material.SetFloat("_GrayscaleAmount", 1);
+            _button.onClick.AddListener(() =>
+            {
+                Purchase();
+                data.OnPurchase();
+            });
+        }
+
+        public void Purchase()
+        {
+            _cost.gameObject.SetActive(false);
+            _iconImage.material.SetFloat("_GrayscaleAmount", 0);
+        }
+    }
+}
---
+++ NecromaticUnity/Assets/Scripts/UI/Research/ResearchManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Necromatic.UI.Research
+{
+	public class ResearchData
+	{
+		public string Name;
+		public int Cost;
+		public string IconPath;
+		public UnityAction OnPurchase;
+
+		public ResearchData(string name, int cost, string iconPath, UnityAction onPurchase)
+		{
+			Name = name;
+			Cost = cost;
+			IconPath = iconPath;
+			OnPurchase = onPurchase;
+		}
+	}
+
+    public class ResearchManager : MonoBehaviour
+    {
+		[SerializeField] private AbilityUI _abilityUIPrefab;
+		[SerializeField] private RectTransform _abilitiesLocation;
+
+		private static string _iconLocation = "Images/UI/Icons/";
+
+		private List<ResearchData> _researchAbilities = new List<ResearchData>()
+		{
+			new ResearchData("Sacrifice", 1, $"{_iconLocation}{"icon_sacrifice"}", () => print("Bought Sacrifice"))
+		};
+
+		void Awake()
+		{
+			foreach(var a in _researchAbilities)
+			{
+				var instance = Instantiate(_abilityUIPrefab);
+				instance.transform.SetParent(_abilitiesLocation);
+				instance.Init(a);
+			}
+		}
+
+		
+    }
+}
