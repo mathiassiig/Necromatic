23b87d0915047e81080c3f6aaa938c10b3c27719
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2018-03-11 15:09:24 +0200

    Simple follow-cam

---
+++ NecromaticUnity/Assets/Scripts/UI/FollowCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Necromatic.UI
+{
+    public class FollowCamera : MonoBehaviour
+    {
+		[SerializeField] private Transform _target;
+		[SerializeField] private Vector3 _offset;
+
+        void Update()
+        {
+			transform.LookAt(_target);
+			transform.position = _target.position + _offset;
+        }
+    }
+}
