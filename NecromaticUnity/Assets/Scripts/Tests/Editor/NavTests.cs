43a4db1a42e58bf6190a01d2824d3c681d18a9fb
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2018-03-13 19:42:38 +0200

    WIP navmesh and tests

---
+++ NecromaticUnity/Assets/Scripts/Tests/Editor/NavTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using Necromatic.World;
+namespace Necromatic.Testing
+{
+    [TestFixture]
+    public class NavTests
+    {
+        [Test]
+        public void GetGridPosTest_FirstQuadrant()
+        {
+            var navMesh = new NavigationMesh();
+            var result = navMesh.GetGridPos(new Vector3(0.75f, 0, 0.75f));
+            Assert.AreEqual(new Vector2Int(1, 1), result);
+        }
+
+        [Test]
+        public void GetGridPosTest_SecondQuadrant()
+        {
+            var navMesh = new NavigationMesh();
+            var result = navMesh.GetGridPos(new Vector3(-0.75f, 0, 0.75f));
+            Assert.AreEqual(new Vector2Int(-2, 1), result);
+        }
+
+        [Test]
+        public void GetGridPosTest_ThirdQuadrant()
+        {
+            var navMesh = new NavigationMesh();
+            var result = navMesh.GetGridPos(new Vector3(-0.75f, 0, -0.75f));
+            Assert.AreEqual(new Vector2Int(-2, -2), result);
+        }
+
+        [Test]
+        public void GetGridPosTest_FourthQuadrant()
+        {
+            var navMesh = new NavigationMesh();
+            var result = navMesh.GetGridPos(new Vector3(0.75f, 0, -0.75f));
+            Assert.AreEqual(new Vector2Int(1, -2), result);
+        }
+    }
+}
---
+++ NecromaticUnity/Assets/Scripts/World/NavigationMesh.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Necromatic.World
+{
+    public enum NavTileStatus
+    {
+        Taken,
+        Free
+    }
+
+    public class NavigationMesh
+    {
+		private const int SUBDIVISION = 2;
+        private Dictionary<Vector2Int, NavTileStatus> _navTiles = new Dictionary<Vector2Int, NavTileStatus>();
+
+		public bool IsAvailable(Vector3 position)
+		{
+			Vector2Int navPos = GetGridPos(position);
+			if(!_navTiles.ContainsKey(navPos))
+			{
+				_navTiles.Add(navPos, NavTileStatus.Free);
+			}
+			return _navTiles[navPos] == NavTileStatus.Free ? true : false;
+		}
+
+		public Vector2Int GetGridPos(Vector3 position)
+		{
+			int closest_grid_x = Mathf.FloorToInt(position.x/(1/(float)SUBDIVISION));
+			int closest_grid_z = Mathf.FloorToInt(position.z/(1/(float)SUBDIVISION));
+			return new Vector2Int(closest_grid_x, closest_grid_z);
+		}
+    }
+}
